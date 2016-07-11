3633d6a654cbb140cdd59d4fbce1f2333530a727
IOURI NEIKOV <neikov@gmail.com>
2016-07-11 19:39:14 +0300

    Level 30 on the go lesson15.big01 task 5 validated;

--- src/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/com/javarush/test/level30/lesson15/big01/Server.java
@@ -1,7 +1,32 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by URAN on 11.07.2016.
  */
 public class Server {
+
+    private static class Handler extends Thread{
+
+        private Socket socket;
+
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket;
+        int port = ConsoleHelper.readInt();
+        try {
+            serverSocket = new ServerSocket(port);
+        }catch (IOException ex) {}
+        ConsoleHelper.writeMessage("Сервер запущен");
+        while (true){
+
+        }
+    }
 }
