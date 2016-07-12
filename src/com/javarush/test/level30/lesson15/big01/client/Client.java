8481c39fdcab9b755eac1c636ea3907bd8f513e8
IOURI NEIKOV <neikov@gmail.com>
2016-07-12 14:48:56 +0300

    Level 30 on the go lesson15.big01 task 13 validated;

--- src/com/javarush/test/level30/lesson15/big01/client/Client.java
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.test.level30.lesson15.big01.client;
 
 import com.javarush.test.level30.lesson15.big01.Connection;
+import com.javarush.test.level30.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level30.lesson15.big01.Message;
+import com.javarush.test.level30.lesson15.big01.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by URAN on 12.07.2016.
@@ -12,5 +17,37 @@ public class Client {
 
     public class SocketThread extends Thread {
 
+    }
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Введите имя пользователя:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSentTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        }catch (IOException exx){
+            System.out.println("Ошибка соединения. Сообщение отослать не удалось");
+            clientConnected = false;
+        }
     }
 }
