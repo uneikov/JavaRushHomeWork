cacb78d34c818f3099f536896e9ae6278ad1435d
IOURI NEIKOV <neikov@gmail.com>
2016-07-19 11:32:33 +0300

    Level 31 on the go big15 task04 validated

--- src/com/javarush/test/level31/lesson15/big01/Archiver.java
+++ src/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level31.lesson15.big01;
 
-import java.io.IOException;
+import com.javarush.test.level31.lesson15.big01.command.ExitCommand;
+
 import java.nio.file.Paths;
 
 /**
@@ -17,17 +18,25 @@ public class Archiver {
             ConsoleHelper.writeMessage("Input full path to file for result archive:");
 
             fileName = ConsoleHelper.readString();
+            //fileName = "C:/Users/URAN/DEsktop/FileTest/lzlz.zip";
             ZipFileManager manager = new ZipFileManager(Paths.get(fileName));
 
             ConsoleHelper.writeMessage("Input full path to file you want to be archived:");
+
             fileName = ConsoleHelper.readString();
+            //fileName = "C:/Users/URAN/Desktop/FileTest/LZ.mp3";
 
             try {
                 manager.createZip(Paths.get(fileName));
                 break;
             }catch (Exception ex){
+                ex.printStackTrace();
                 ConsoleHelper.writeMessage("Invalid input. Try one`s more");
             }
         }
+
+        try {
+            new ExitCommand().execute();
+        }catch (Exception ex) {}
     }
 }
