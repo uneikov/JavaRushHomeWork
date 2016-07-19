e064d47f1cbc3ceb69c2a71778d207e5d7a215bc
IOURI NEIKOV <neikov@gmail.com>
2016-07-19 11:39:07 +0300

    Level 31 on the go big15 task05 validated

---
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level31.lesson15.big01.command;
+
+/**
+ * Created by URAN on 19.07.2016.
+ */
+public abstract class ZipCommand implements Command{
+}
---
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipExtractCommand.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level31.lesson15.big01.command;
+
+/**
+ * Created by URAN on 19.07.2016.
+ */
+public class ZipExtractCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
