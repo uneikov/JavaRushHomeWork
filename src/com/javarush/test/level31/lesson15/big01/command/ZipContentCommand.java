32f0ed177de39b3bd33815523a069c8266fc60cd
IOURI NEIKOV <neikov@gmail.com>
2016-07-20 12:39:28 +0300

    Level 31 on the go big15 task13 validated

--- src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
@@ -1,5 +1,11 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.FileProperties;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by URAN on 19.07.2016.
  */
@@ -7,5 +13,7 @@ public class ZipContentCommand extends ZipCommand{
     @Override
     public void execute() throws Exception {
 
+        //List<FileProperties> list = new ZipFileManager(Paths.get("C:/Users/URAN/DEsktop/ZZZ/zzz.zip")).getFilesList();
+        //for (FileProperties props : list) System.out.println(props.toString());
     }
 }
