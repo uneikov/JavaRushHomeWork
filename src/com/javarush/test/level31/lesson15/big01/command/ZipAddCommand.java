3c78baf10abaa7548a6d53345cdf9e9c262f88ef
IOURI NEIKOV <neikov@gmail.com>
2016-07-23 17:23:32 +0300

    Level 31 on the go big15 task17 validated

--- src/com/javarush/test/level31/lesson15/big01/command/ZipAddCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipAddCommand.java
@@ -1,11 +1,32 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by URAN on 19.07.2016.
  */
 public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
 
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.addFile(sourcePath);
+
+            ConsoleHelper.writeMessage("Добавление в архив завершено.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Файл не был найден.");
+        }
     }
 }
