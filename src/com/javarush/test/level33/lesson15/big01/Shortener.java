5a25125ad417e3666ef17193d4ebbbd79cf56f29
IOURI NEIKOV <neikov@gmail.com>
2016-07-31 14:25:41 +0300

    Level 33 on the go big15 task 2 validated

---
+++ src/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level33.lesson15.big01;
+
+/**
+ * Created by URAN on 31.07.2016.
+ */
+public class Shortener {
+    public Long getId(String string){
+        return null;
+    }
+
+    public String getString(Long id){
+        return "";
+    }
+}
