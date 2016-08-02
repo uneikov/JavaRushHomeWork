b51c9d6067f5f501a922bcf7c8c24072c6af846a
IOURI NEIKOV <neikov@gmail.com>
2016-08-02 19:01:12 +0300

    Level 33 on the go big15 task 7 validated

---
+++ src/com/javarush/test/level33/lesson15/big01/strategies/Entry.java
@@ -0,0 +1,48 @@
+package com.javarush.test.level33.lesson15.big01.strategies;
+
+import java.io.Serializable;
+
+/**
+ * Created by URAN on 02.08.2016.
+ */
+public class Entry implements Serializable {
+
+    Long key;
+    String value;
+    Entry next;
+    int hash;
+
+    public Entry(int hash, Long key, String value, Entry next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public Long getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        result = 31 * result + hash;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "key=" + key +
+                ", value='" + value + '\'' +
+                ", next=" + next +
+                ", hash=" + hash +
+                '}';
+    }
+}
